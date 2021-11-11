e6edb52383a7b3ce0a6ed09010dc5188b17b358f
Okami2612 <51777169+Okami2612@users.noreply.github.com>
2021-11-11 18:04:55 +0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,93 @@
+// ===== Login form =====
+let loginForm = document.querySelector(".header .login-form");
+
+document.querySelector("#login-btn").onclick = () => {
+  loginForm.classList.toggle("active");
+  navbar.classList.remove("active");
+};
+
+// ===== Show menu =====
+let navbar = document.querySelector(".header .navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  loginForm.classList.remove("active");
+};
+
+window.onscroll = () => {
+  loginForm.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+// ===== particles.js =====
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80, density: { enable: true, value_area: 800 } },
+    color: { value: "#5f22cf" },
+    shape: {
+      type: "circle",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 5 },
+      image: { src: "img/github.svg", width: 100, height: 100 },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: false, speed: 40, size_min: 0.1, sync: false },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#2a49d9",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 1200 },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "grab" },
+      onclick: { enable: true, mode: "push" },
+      resize: true,
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+      repulse: { distance: 200, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 },
+    },
+  },
+  retina_detect: true,
+});
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+})
+
+sr.reveal(`.home_data`)
+sr.reveal(`.home_img`, {delay: 500})
+sr.reveal(`.home_socials`, {delay: 600})
+sr.reveal(`.about_img, .contact_box`,{origin: 'left'})
+sr.reveal(`.about_data, .contact_form`,{origin: 'right'})
+sr.reveal(`.steps_card, .product_card, .questions_group, .footer`,{interval: 100})
